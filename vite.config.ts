ba2f18fa1129ca77c6afe092eb219b097ccda59a
Adribv <arbaazsms04@gmail.com>
2026-06-28 03:04:07 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+
 
 export default defineConfig({
   plugins: [
     tsconfigPaths({ projects: ["./tsconfig.json"] }),
+    TanStackRouterVite(),
     tailwindcss(),
     tanstackStart(),
     react(),
